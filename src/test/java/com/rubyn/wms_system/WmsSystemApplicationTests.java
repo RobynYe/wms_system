61306b453d299f5f1ed461b9b65e6017d1c45731
Rubyn <18146563086@163.com>
2022-05-19 11:58:27 +0800

    Initial commit

---
+++ src/test/java/com/rubyn/wms_system/WmsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubyn.wms_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
