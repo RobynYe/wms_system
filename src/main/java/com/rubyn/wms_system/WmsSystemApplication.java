a3cb0547dd93842bc991148ce324bc8ef8691857
RobynYe <101983690+RobynYe@users.noreply.github.com>
2022-05-19 15:01:22 +0800

    Update WmsSystemApplication.java

--- src/main/java/com/rubyn/wms_system/WmsSystemApplication.java
+++ src/main/java/com/rubyn/wms_system/WmsSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WmsSystemApplication {
-
+//修改1
     public static void main(String[] args) {
         SpringApplication.run(WmsSystemApplication.class, args);
     }
